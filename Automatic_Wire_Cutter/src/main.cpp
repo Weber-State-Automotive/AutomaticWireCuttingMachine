890313b071ea79485ef2900df9c459fdc1287764
Scott Hadzik <scotthadzik@weber.edu>
2021-06-03 18:43:24 -0600

    removed search for identifier for lcd

--- Automatic_Wire_Cutter/src/main.cpp
+++ Automatic_Wire_Cutter/src/main.cpp
@@ -35,27 +35,6 @@
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
 
-// Color definitions
-#define ILI9341_BLACK       0x0000      /*   0,   0,   0 */
-#define ILI9341_NAVY        0x000F      /*   0,   0, 128 */
-#define ILI9341_DARKGREEN   0x03E0      /*   0, 128,   0 */
-#define ILI9341_DARKCYAN    0x03EF      /*   0, 128, 128 */
-#define ILI9341_MAROON      0x7800      /* 128,   0,   0 */
-#define ILI9341_PURPLE      0x780F      /* 128,   0, 128 */
-#define ILI9341_OLIVE       0x7BE0      /* 128, 128,   0 */
-#define ILI9341_LIGHTGREY   0xC618      /* 192, 192, 192 */
-#define ILI9341_DARKGREY    0x7BEF      /* 128, 128, 128 */
-#define ILI9341_BLUE        0x001F      /*   0,   0, 255 */
-#define ILI9341_GREEN       0x07E0      /*   0, 255,   0 */
-#define ILI9341_CYAN        0x07FF      /*   0, 255, 255 */
-#define ILI9341_RED         0xF800      /* 255,   0,   0 */
-#define ILI9341_MAGENTA     0xF81F      /* 255,   0, 255 */
-#define ILI9341_YELLOW      0xFFE0      /* 255, 255,   0 */
-#define ILI9341_WHITE       0xFFFF      /* 255, 255, 255 */
-#define ILI9341_ORANGE      0xFD20      /* 255, 165,   0 */
-#define ILI9341_GREENYELLOW 0xAFE5      /* 173, 255,  47 */
-#define ILI9341_PINK        0xF81F
-
 /******************* UI details */
 #define BUTTON_X 300
 #define BUTTON_Y 155
@@ -91,11 +70,6 @@
 #define GRN_LED 49
 #define BLU_LED 47
 
-// Define button state variables
-boolean RED_state = 0;
-boolean GRN_state = 0;
-boolean BLU_state = 0;
-
 // Define button array object
 Adafruit_GFX_Button buttons[3];
 
@@ -111,58 +85,17 @@ MCUFRIEND_kbv tft;
 TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 
 
-
-
-
 void setup(void) {
 
   Serial.begin(9600);
   Serial.println(F("TFT LCD test"));
 
-  pinMode(RED_LED, OUTPUT);
-  pinMode(GRN_LED, OUTPUT);
-  pinMode(BLU_LED, OUTPUT);
 
 
   tft.reset();
 
-  uint16_t identifier = tft.readID();
-  if (identifier == 0x9325) {
-    Serial.println(F("Found ILI9325 LCD driver"));
-  } else if (identifier == 0x9328) {
-    Serial.println(F("Found ILI9328 LCD driver"));
-  } else if (identifier == 0x4535) {
-    Serial.println(F("Found LGDP4535 LCD driver"));
-  } else if (identifier == 0x7575) {
-    Serial.println(F("Found HX8347G LCD driver"));
-  } else if (identifier == 0x9341) {
-    Serial.println(F("*Found ILI9341 LCD driver"));
-  } else if (identifier == 0x7783) {
-    Serial.println(F("Found ST7781 LCD driver"));
-  } else if (identifier == 0x8230) {
-    Serial.println(F("Found UC8230 LCD driver"));
-  }
-  else if (identifier == 0x8357) {
-    Serial.println(F("Found HX8357D LCD driver"));
-  } else if (identifier == 0x0101)
-  {
-    identifier = 0x9341;
-    Serial.println(F("Found 0x9341 LCD driver"));
-  } else if (identifier == 0x9481)
-  {
-    Serial.println(F("Found 0x9481 LCD driver"));
-  }
-  else if (identifier == 0x9486)
-  {
-    Serial.println(F("Found 0x9486 LCD driver"));
-  }
-  else {
-    Serial.print(F("Unknown LCD driver chip: "));
-    Serial.println(identifier, HEX);
-    identifier = 0x9486;
-  }
-
   // Setup the Display
+  uint16_t identifier = 0x9486;
   tft.begin(identifier);
   Serial.print("TFT size is "); Serial.print(tft.height()); Serial.print("x"); Serial.println(tft.width());
   tft.setRotation(3);
