0cdfa2097d719288dccb3ed1b6df960c51e769d8
Scott Hadzik <scotthadzik@weber.edu>
2021-06-19 23:42:44 -0600

    color and y shift

--- Automatic_Wire_Cutter/src/main.cpp
+++ Automatic_Wire_Cutter/src/main.cpp
@@ -75,7 +75,7 @@ Adafruit_GFX_Button buttons[2];
 
 // Define arrays with button text and colors
 const char* buttonlabels[3][6] ={ { "-1", "+1", "-1", "+1", "-1", "+1"}, { "-10", "+10"}, { "-100", "+100"} };
-uint16_t buttoncolors[6] = {RED, GREEN, BLUE};
+uint16_t buttoncolors[6] = {RED, GREEN, BLUE, YELLOW, GREY, CYAN};
 
 // Define Menu Array
 String menu_titles[3] = {
@@ -138,7 +138,7 @@ void setupTouchscreen(){
   
   // Draw buttons
   for (uint8_t row = 0; row < 3; row++){
-    int y_coord = BUTTON_Y + row * 50;
+    int y_coord = BUTTON_Y + row * 40;
     for (uint8_t col = 0; col < 6; col++) {
 
         int x_coord = BUTTON_X + col * (90);
