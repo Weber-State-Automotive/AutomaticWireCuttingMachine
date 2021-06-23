689810025e03fa3f1cbc88a046dce87964eb2a5e
Scott Hadzik <scotthadzik@weber.edu>
2021-06-22 21:07:34 -0600

    created text function

--- Automatic_Wire_Cutter/src/main.cpp
+++ Automatic_Wire_Cutter/src/main.cpp
@@ -1,12 +1,27 @@
+/**================================================================================================
+ * ?                                           ABOUT
+ * @author         :  Scott Hadzik
+ * @email          :  scotthadzik@weber.edu
+ * @repo           :  https://github.com/Weber-State-Automotive/AutomaticWireCuttingMachine
+ * @createdOn      :  March 2019
+ * @description    :  Wire Cutting and Stripping Machine. Ada 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @credit         :  DJ Harrigan Element 14 Presents
+ *                        https://www.element14.com/community/docs/DOC-91289/l/episode-368-arduino-automatic-wire-cutter-and-stripper?CMP=SOM-YOUTUBE-E14PRESENTS-EP368-DESC
+ *                    Adafruit phone screen demo 
+ *                        https://learn.adafruit.com/adafruit-2-8-tft-touch-shield-v2/resistive-touchscreen-paint-demo
+ *                    DroneBot Workshop 
+ *                        https://dronebotworkshop.com
+ *                    Roderek Soest wrote most of the first version of this program
+ *                        https://github.com/roderek/E14-wire-cutter-for-Weber-Automotive
+ *================================================================================================**/
+
+
 #include <Arduino.h>
-/*
-  Modified from Adafruit phone screen demo
-  DroneBot Workshop 2019
-  https://dronebotworkshop.com
-
-  Modified from dronebotworkshop.com for WSU wire cutting maching
-*/
-
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_TFTLCD.h> // LCD library
 #include <TouchScreen.h> // Touchscreen Library
@@ -52,15 +67,6 @@
 #define STATUS_X 10
 #define STATUS_Y 65
 
-//Define button states
-const char* current_button_state_list[3] = {"length", "qty", "strip"};
-const char* current_menu_state = current_button_state_list[0];
-
-//Define button array object
-Adafruit_GFX_Button unit_buttons[6];
-Adafruit_GFX_Button number_buttons[3];
-Adafruit_GFX_Button menu_buttons[3];
-
 // Define object for TFT (LCD)display
 MCUFRIEND_kbv tft;
 
@@ -70,7 +76,16 @@ TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 
 #define PIN_SENSOR A8 // Hall effect sensor for determining position of cutter (RED wire to 5V, BLACK wire to GND, BLUE wire to A8)
 
-// ----------Cut Stepper ---------- //
+/**========================================================================
+ **                           Adafruit Button variables
+ *========================================================================**/
+Adafruit_GFX_Button unit_buttons[6];
+Adafruit_GFX_Button number_buttons[3];
+Adafruit_GFX_Button menu_buttons[3];
+
+/**========================================================================
+ **                           Cutting Stepper variables
+ *========================================================================**/
 #define CUT_DIR_PIN 35
 #define CUT_STP_PIN 43
 AccelStepper CUT_stepper(1, CUT_STP_PIN, CUT_DIR_PIN);
@@ -81,17 +96,35 @@ int delta_time = 100;
 long retracted_cut_position = 1700; 
 boolean cut_stepper_is_homed = false;
 
-// ----------Feed Stepper ---------- //
+/**========================================================================
+ **                           Text field variables
+ *========================================================================**/
+#define TEXT_X 20
+#define TEXT_Y 20
+#define TEXT_TSIZE 6
+int wire_length = 0;
+int wire_qty = 1;
+int wire_strip_length = 10;
+int cut_values [3] = {wire_length, wire_qty, wire_strip_length};
+
+/**========================================================================
+ **                           Feed stepper variables
+ *========================================================================**/
 #define FEED_PIN_ENABLE 25
 #define FEED_DIR_PIN 29
 #define FEED_STP_PIN 27
 AccelStepper FEED_stepper(1, FEED_STP_PIN, FEED_DIR_PIN);
 float feed_current_pos = 0;
 
-// ---------Custom Numbers ---------//
-int wire_length = 0;
-int wire_qty = 1;
-int strip_size = 10;
+
+/**========================================================================
+ **                           setButtonState
+ *?  Inverts the button to show which one is selected
+ *@param activeButton --> Invert this button  
+ *@param inactiveButton1 --> draw normal button state  
+ *@param inactiveButton2 --> draw normal button state
+ *@return void
+ *========================================================================**/
 
 void setButtonState(int activeButton, int inactiveButton1, int inactiveButton2){
   menu_buttons[activeButton].drawButton(true);
@@ -99,17 +132,64 @@ void setButtonState(int activeButton, int inactiveButton1, int inactiveButton2){
   menu_buttons[inactiveButton2].drawButton();
 }
 
+ /**========================================================================
+  **                           setTextValue
+  *?  Sets the value of the text field based on the place
+  *@param value int the value to set  
+  *@param place int the place in the array 
+                          0 -> length
+                          1 -> qty
+                          2 -> strip   
+  *@return void
+  *========================================================================**/
+void setTextValue(int value, int place){
+  
+    tft.setCursor(TEXT_X + 2 + (place*(160)), TEXT_Y+10);
+    tft.setTextColor(WHITE, BLACK);
+    tft.setTextSize(TEXT_TSIZE);
+    if (value < 10){
+      tft.print("  ");
+    }
+    else if (value < 100){
+      tft.print(" ");
+    }
+    tft.print(value);
+}
+
+/**========================================================================
+ **                           setupTouchscreen
+ *?  Draws the buttons and textfields for the touchscreen 
+ *@return void
+ *========================================================================**/
+
 void setupTouchscreen(){
   Serial.println("Setting up Touchscreen...");
   
-  // ---------- Setup the Touchscreen Display ---------- //
+  /**========================================================================
+   *                           Setup the Touchscreen Display
+   *========================================================================**/
   tft.reset();
   uint16_t identifier = 0x9486;
   tft.begin(identifier);
   tft.setRotation(3);
   tft.fillScreen(BLACK);
+  //Define button states
+  const char* current_button_state_list[3] = {"length", "qty", "strip"};
+  const char* current_menu_state = current_button_state_list[0];
+
+  /**========================================================================
+   *                           Setup the text field
+   * 
+   *  ? There are three text fields
+   *  TODO initialize fields with preset values
+   *========================================================================**/
+  setTextValue(wire_length,0);
+  setTextValue(wire_qty,1);
+  setTextValue(wire_strip_length,2);
   
-  /******************* UI details */
+  /* -------------------------------------------------------------------------- */
+  /*                                Unit buttons                                */
+  /* -------------------------------------------------------------------------- */
   #define BUTTON_X 40
   #define BUTTON_Y 180
   #define BUTTON_W 60
@@ -119,63 +199,12 @@ void setupTouchscreen(){
   #define BUTTON_SPACING_Y 30
   #define BUTTON_TEXTSIZE 3
   
-  // Define arrays with button text and colors
   const char* buttonlabels[3][2] ={ 
       { "-1", "+1"}, 
       { "-10", "+10"}, 
       { "-50", "+50"} };
   uint16_t buttoncolors[6] = {RED, GREEN, BLUE, YELLOW, GREY, CYAN};
 
-
-  #define TITLE_X 80
-  #define TITLE_Y 120
-  int TITLE_W = (BUTTON_W * 2) + BUTTON_Padding;
-  int TITLE_H = BUTTON_H;
-  int TITLE_SPACING_X = TITLE_W + BUTTON_Padding;
-  int TITLE_Padding = BUTTON_Padding;
-  int TITLE_TEXTSIZE = 3;
-
-  // ----------Text Box ---------- //
-  #define TEXT_X 20
-  #define TEXT_Y 20
-  #define TEXT_TSIZE 6
-  int TEXT_W = TITLE_W ;
-  int TEXT_H = BUTTON_H *2;
-  int TEXT_SPACING_X = TITLE_SPACING_X;
-  int TEXT_PADDING = BUTTON_Padding;
-  uint8_t textfield_i=0;
-  const char* text_labels[3] = { "000", "000", "000"};
-
-  // Define arrays with button text and colors
-  const char* menu_labels[3] = { "Length", "Qty", "Strip"};
-  uint16_t title_colors[3] = {RED, GREY, BLUE};
-
-
-  for (uint8_t col = 0; col < 3; col++) {
-    tft.setCursor(TEXT_X + 2 + (col*(160)), TEXT_Y+10);
-    tft.setTextColor(WHITE, BLACK);
-    tft.setTextSize(TEXT_TSIZE);
-    tft.print("  0");
-  }
-  for (uint8_t col = 0; col < 3; col++) {
-
-        int x_coord = TITLE_X + col * (TITLE_W + TITLE_Padding);
-        menu_buttons[col].initButton(&tft, 
-                                x_coord,
-                                TITLE_Y, // x, y, w, h, outline, fill, text
-                                TITLE_W, 
-                                TITLE_H, 
-                                WHITE, 
-                                title_colors[col],
-                                WHITE,
-                                menu_labels[col], 
-                                BUTTON_TEXTSIZE);
-        menu_buttons[col].drawButton();
-  }
-
-  setButtonState(0,1,2); //Make length the active button
-
-  // Draw buttons
   int function_button = 0;
   for (uint8_t row = 0; row < 3; row++){
     int y_coord = BUTTON_Y + row * 50;
@@ -197,11 +226,44 @@ void setupTouchscreen(){
     }
     
   }
-  
+  /* -------------------------------------------------------------------------- */
+  /*                                Menu buttons                                */
+  /* -------------------------------------------------------------------------- */
+  #define TITLE_X 80
+  #define TITLE_Y 120
+  int TITLE_W = (BUTTON_W * 2) + BUTTON_Padding;
+  int TITLE_H = BUTTON_H;
+  int TITLE_SPACING_X = TITLE_W + BUTTON_Padding;
+  int TITLE_Padding = BUTTON_Padding;
+  int TITLE_TEXTSIZE = 3;
+  const char* menu_labels[3] = { "Length", "Qty", "Strip"};
+  uint16_t title_colors[3] = {RED, GREY, BLUE};
 
+
+  for (uint8_t col = 0; col < 3; col++) {
+
+        int x_coord = TITLE_X + col * (TITLE_W + TITLE_Padding);
+        menu_buttons[col].initButton(&tft, 
+                                x_coord,
+                                TITLE_Y, // x, y, w, h, outline, fill, text
+                                TITLE_W, 
+                                TITLE_H, 
+                                WHITE, 
+                                title_colors[col],
+                                WHITE,
+                                menu_labels[col], 
+                                BUTTON_TEXTSIZE);
+        menu_buttons[col].drawButton();
+  }
+
+/* ---------------------- Make length the active button --------------------- */
+  setButtonState(0,1,2); //
   Serial.println("Touchscreen Setup");
 }
 
+/**========================================================================
+ **                           Setup Cutting Stepper Motor
+ *========================================================================**/
 void setupCutStepper(){
   Serial.println("Setting up Stepper Cut...");
   // Setup the cutting stepper motor
@@ -211,6 +273,9 @@ void setupCutStepper(){
   Serial.println("Cut Stepper Setup");
 }
 
+/**========================================================================
+ **                           Setup Feed Stepper Motor
+ *========================================================================**/
 void setupFeedStepper(){
   Serial.println("Setting up Feed Stepper...");
   
@@ -224,10 +289,11 @@ void setupFeedStepper(){
   Serial.println("Feed Stepper Setup");
 }
 
+/**========================================================================
+ **                          Menu Selection
+ *========================================================================**/
 
-
-
-int setMenu(uint8_t b){
+int setMenuSelection(uint8_t b){
   if (b == 0) { // length   
     setButtonState(0,1,2);
     return 0;
@@ -241,8 +307,10 @@ int setMenu(uint8_t b){
     return 2;
   }
 }
-
-void setUnit(uint8_t function_button){
+/**========================================================================
+ **                           Set which multiple to use
+ *========================================================================**/
+void setMultiple(uint8_t function_button){
     // Serial.println(function_button);
     switch (function_button){
       case 0:
@@ -271,11 +339,9 @@ void setUnit(uint8_t function_button){
     
 }
 
-
 void setup(void) {
+  Serial.begin(9600);  
 
-  Serial.begin(9600);
-  
   setupTouchscreen();
   setupCutStepper();
   setupFeedStepper();
@@ -304,7 +370,7 @@ void loop() {
   for (uint8_t b = 0; b < 3; b++) {
     if ((menu_buttons[b].contains(p.y, p.x)) && p.x > 10){
       menu_buttons[b].press(true);  // tell the button it is pressed
-      setMenu(b);
+      setMenuSelection(b);
     }
   }
 
@@ -314,7 +380,7 @@ void loop() {
       Adafruit_GFX_Button btn = unit_buttons[function_button];
       if ((btn.contains(p.y, p.x)) && p.x > 10){
         btn.press(true);  // tell the button it is pressed
-        setUnit(function_button);
+        setMultiple(function_button);
         tft.setCursor(TEXT_X + 2, TEXT_Y+10);
         tft.setTextColor(WHITE, BLACK);
         tft.setTextSize(TEXT_TSIZE);
